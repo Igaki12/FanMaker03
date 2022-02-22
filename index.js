e357c1128b7dfecb5aa41194dddd1794850d6e98
unknown <igatatsu1997@gmail.com>
2022-02-22 17:10:36 +0900

    角度調節機能を追加

--- index.js
+++ index.js
@@ -9,13 +9,14 @@ const startPanel = document.getElementById('start-panel');
 const editFontPanel = document.getElementById('edit-font-panel');
 const editBackgroundBtn = document.getElementById('edit-background-btn');
 const createFontBtn = document.getElementById('create-font-btn');
+const editAnglePanel = document.getElementById('edit-angle-panel');
+const editAngleBack = document.getElementById('edit-angle-back-btn');
 console.log("あ");
 const backStartPanel = () => {
   startPanel.style = "display: display;";
   editFontPanel.style = "display: none";
   editBackgroundPanel.style = "display: none";
 };
-
 const openBackgroundDetailPanel = () => {
   additionalBackgroundPanel.style = "display: display;";
   additionalBackgroundOpenBtn.style = "display: none;";
@@ -37,10 +38,19 @@ const openBackgroundPanel = () => {
   editBackgroundPanel.style = "display: display;";
   startPanel.style = "display: none;";
 }
+const openEditAnglePanel = () => {
+  editAnglePanel.style = "display: display";
+  editFontPanel.style = "display:none";
+}
+const closeEditAnglePanel = () => {
+  editAnglePanel.style = "display: none";
+  editFontPanel.style = "display:display";
+}
 createFontBtn.addEventListener('click',openEditFontPanel);
 editBackgroundBtn.addEventListener('click',openBackgroundPanel);
 editFontBack.addEventListener('click',backStartPanel);
 editBackgroundBack.addEventListener('click',backStartPanel);
+editAngleBack.addEventListener('click',closeEditAnglePanel);
 additionalBackgroundOpenBtn.addEventListener('click',() => {
   setTimeout(openBackgroundDetailPanel,100);
   editBackgroundBack.removeEventListener('click',backStartPanel);
