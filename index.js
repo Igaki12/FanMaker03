947b841635da337e2e4f647d7ee3b950f437819a
unknown <igatatsu1997@gmail.com>
2022-02-23 11:33:33 +0900

    fontcolor,fontsize編集画面を表示できるように追加

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const additionalBackgroundOpenBtn = document.getElementById('additional-backgrou
 const additionalBackgroundCloseBtn = document.getElementById('additional-background-close-btn');
 const additionalBackgroundRow = document.getElementById('additional-background-row');
 const additionalBackgroundPanel = document.getElementById('additional-background-panel');
+const additionalFontcolorOpenBtn = document.getElementById('additional-fontcolor-open-btn');
+const additionalFontcolorCloseBtn = document.getElementById('additional-fontcolor-close-btn');
+const additionalFontcolorRow = document.getElementById('additional-fontcolor-row');
+const additionalFontcolorPanel = document.getElementById('additional-fontcolor-panel');
 const editBackgroundBack = document.getElementById('edit-background-back-btn');
 const editFontBack = document.getElementById('edit-font-back-btn');
 const editBackgroundPanel = document.getElementById('edit-background-panel');
@@ -10,8 +14,15 @@ const editFontPanel = document.getElementById('edit-font-panel');
 const editBackgroundBtn = document.getElementById('edit-background-btn');
 const createFontBtn = document.getElementById('create-font-btn');
 const editAnglePanel = document.getElementById('edit-angle-panel');
+const editFontcolorPanel = document.getElementById('edit-fontcolor-panel');
+const editFontsizePanel = document.getElementById('edit-fontsize-panel');
 const editAngleBack = document.getElementById('edit-angle-back-btn');
+const fontsizeBack = document.getElementById('edit-fontsize-back-btn');
+const fontcolorBack = document.getElementById('edit-fontcolor-back-btn');
 const angleBtn = document.getElementById('angle-btn');
+const fontcolorBtn = document.getElementById('fontcolor-btn');
+const fontsizeBtn = document.getElementById('fontsize-btn');
+
 console.log("あ");
 const backStartPanel = () => {
   startPanel.style = "display: display;";
@@ -47,12 +58,32 @@ const closeEditAnglePanel = () => {
   editAnglePanel.style = "display: none";
   editFontPanel.style = "display:display";
 }
+const openEditFontsizePanel = () => {
+  editFontsizePanel.style = "display: display";
+  editFontPanel.style = "display: none";
+}
+const closeEditFontsizePanel = () => {
+  editFontsizePanel.style = "display: none";
+  editFontPanel.style = "display: display";
+}
+const openEditFontcolorPanel = () => {
+  editFontcolorPanel.style = "display: display";
+  editFontPanel.style = "display: none";
+}
+const closeEditFontcolorPanel = () => {
+  editFontcolorPanel.style = "display: none";
+  editFontPanel.style = "display: display";
+}
 createFontBtn.addEventListener('click',openEditFontPanel);
 editBackgroundBtn.addEventListener('click',openBackgroundPanel);
 editFontBack.addEventListener('click',backStartPanel);
 editBackgroundBack.addEventListener('click',backStartPanel);
 editAngleBack.addEventListener('click',closeEditAnglePanel);
 angleBtn.addEventListener('click',openEditAnglePanel);
+fontsizeBtn.addEventListener('click',openEditFontsizePanel);
+fontsizeBack.addEventListener('click',closeEditFontsizePanel);
+fontcolorBtn.addEventListener('click',openEditFontcolorPanel);
+fontcolorBack.addEventListener('click',closeEditFontcolorPanel);
 additionalBackgroundOpenBtn.addEventListener('click',() => {
   setTimeout(openBackgroundDetailPanel,100);
   editBackgroundBack.removeEventListener('click',backStartPanel);
