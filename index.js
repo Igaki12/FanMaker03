78778402593239ac6c921b850f51b9dd1158c9f6
unknown <igatatsu1997@gmail.com>
2022-02-24 10:30:41 +0900

    added the function to slide the circle-range

--- index.js
+++ index.js
@@ -133,7 +133,38 @@ fontframeUp.addEventListener('click',() => {
   if(parseInt(fontframeNumber.textContent) > 4) return;
   fontframeNumber.textContent = parseInt(fontframeNumber.textContent) + 1;
 })
-
-
+// 円形スライダーの挙動
+(function () {
+  // var $container = document.getElementById('circle-range');
+  var $slider = document.getElementById('circle-slider');
+  var sliderW2 = $slider.width()/2;
+  var sliderH2 = $slider.height()/2;    
+  var radius = 15;
+  var deg = 0;    
+  var elP = $('#circle-range').offset();
+  var elPos = { x: elP.left, y: elP.top};
+  var X = 0, Y = 0;
+  var mdown = false;
+  $('#circle-range')
+  .mousedown(function (e) { mdown = true; })
+  .mouseup(function (e) { mdown = true; })
+  .mousemove(function (e) {
+      if (mdown) {
+         var mPos = {x: e.clientX-elPos.x, y: e.clientY-elPos.y};
+         var atan = Math.atan2(mPos.x-radius, mPos.y-radius);
+         deg = -atan/(Math.PI/180) + 180; // final (0-360 positive) degrees from mouse position 
+          
+         X = Math.round(radius* Math.sin(deg*Math.PI/180));    
+         Y = Math.round(radius*  -Math.cos(deg*Math.PI/180));
+         $slider.css({ left: X+radius-sliderW2, top: Y+radius-sliderH2 });              
+         // AND FINALLY apply exact degrees to ball rotation
+         $slider.css({ WebkitTransform: 'rotate(' + deg + 'deg)'});
+         $slider.css({ '-moz-transform': 'rotate(' + deg + 'deg)'});
+         //
+         // PRINT DEGREES
+         $('input[name="angle"]').val(Math.ceil(deg));
+      }
+  });
+})();
 
 
