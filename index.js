e2ca5bea6f58fb469a781c3bda9871ffb8e80ff3
unknown <igatatsu1997@gmail.com>
2022-02-24 22:16:43 +0900

    fixed

--- index.js
+++ index.js
@@ -142,7 +142,7 @@ fontframeUp.addEventListener('click',() => {
   var radius = 15;
   var deg = 0;    
   var elP = document.getElementById('circle-range');
-  var elPos = { x: elP.style.marginLeft.split("p")[0], y: elP.style.marginTop.split('px')[0]};
+  var elPos = { x: 30, y: 0};
   var X = 0, Y = 0;
   var mdown = false;
   document.getElementById('circle-range').addEventListener('mousedown',(e) => mdown = true);
