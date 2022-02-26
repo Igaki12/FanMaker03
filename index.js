73756ffe582d29193f0f7dc8554abd4317f66bdb
unknown <igatatsu1997@gmail.com>
2022-02-26 14:44:12 +0900

    fixed

--- index.js
+++ index.js
@@ -91,9 +91,9 @@ const reloadBackgroundDetailColorRange = (colorText) => {
   document.getElementById('rs-bullet-red').innerHTML = redValue;
   document.getElementById('rs-bullet-green').innerHTML = greenValue;
   document.getElementById('rs-bullet-blue').innerHTML = blueValue;
-  document.getElementById('rs-range-line-red').ariaValueNow = redValue;
-  document.getElementById('rs-range-line-green').ariaValueNow = greenValue;
-  document.getElementById('rs-range-line-blue').ariaValueNow = blueValue;
+  document.getElementById('rs-range-line-red').value = redValue;
+  document.getElementById('rs-range-line-green').value = greenValue;
+  document.getElementById('rs-range-line-blue').value = blueValue;
   console.log(`${colorText} = rgb(${redValue},${greenValue},${blueValue})`);
 };
 const backStartPanel = () => {
