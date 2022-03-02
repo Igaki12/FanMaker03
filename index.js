2c4f6013de877d481a94a0f1964d212471c1e7e4
unknown <igatatsu1997@gmail.com>
2022-03-02 15:32:16 +0900

    fixed

--- index.js
+++ index.js
@@ -353,7 +353,7 @@ drawCanvasLogo(logo,"あ","MS ゴシック",80,1,6,"rgb(255,255,255)",0);
 logo = document.getElementById('thickness-logo').getContext("2d");
 drawCanvasLogo(logo,"あ","fantasy",80,0,15,"rgb(127,127,127)",0);
 logo = document.getElementById('font-color-logo').getContext("2d");
-drawCanvasLogo(logo,"あ","fantasy",80,0,6,"linear-gradient(70deg, blue, pink)",0);
+drawCanvasLogo(logo,"あ","fantasy",80,0,6,"rgb(255,63,63)",0);
 logo = document.getElementById('font-size-logo').getContext("2d");
 drawCanvasLogo(logo,"あ","fantasy",50,0,6,"rgb(127,127,127)",0);
 logo = document.getElementById('font-angle-logo').getContext("2d");
