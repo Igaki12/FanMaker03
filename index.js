f69907acee9723f304000a32f0adf61aef37a76c
unknown <igatatsu1997@gmail.com>
2022-03-02 14:41:42 +0900

    added thickness-logo

--- index.js
+++ index.js
@@ -347,8 +347,10 @@ function drawCanvasLogo(context,text,font,size,stroke,thickness,color,angle){
 }
 let logo = document.getElementById('font-type-logo').getContext("2d");
 drawCanvasLogo(logo,"あ","MS 明朝",80,0,6,"rgb(127,127,127)",0);
-let logo2 = document.getElementById('font-frame-logo').getContext("2d");
-drawCanvasLogo(logo2,"あ","MS ゴシック",80,1,6,"rgb(255,255,255)",0);
+logo = document.getElementById('font-frame-logo').getContext("2d");
+drawCanvasLogo(logo,"あ","MS ゴシック",80,1,6,"rgb(255,255,255)",0);
+logo = document.getElementById('thickness-logo').getContext("2d");
+drawCanvasLogo(logo,"あ","MS 明朝",80,0,15,"rgb(127,127,127)",0);
 
 
 
